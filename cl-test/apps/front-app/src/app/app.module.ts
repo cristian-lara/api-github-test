7c408fb63aedfb103fcc2df29fb4b8ec78c7f00c
Cristian Lara <cristian89lara@gmail.com>
2022-10-28 21:36:58 -0500

    update: app module updated

--- cl-test/apps/front-app/src/app/app.module.ts
+++ cl-test/apps/front-app/src/app/app.module.ts
@@ -3,9 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomeComponent } from './routes/home/home.component';
+import { RouteCommitsComponent } from './routes/route-commits/route-commits.component';
+import { TableCommitsComponent } from './components/table-commits/table-commits.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    HomeComponent,
+    RouteCommitsComponent,
+    TableCommitsComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
