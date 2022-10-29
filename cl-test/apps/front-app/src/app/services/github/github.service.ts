64ece454ead181e71759de657b4e6de3b6e64b96
Cristian Lara <cristian89lara@gmail.com>
2022-10-29 12:02:15 -0500

    feat: service github get hsitory commits

--- cl-test/apps/front-app/src/app/services/github/github.service.ts
+++ cl-test/apps/front-app/src/app/services/github/github.service.ts
@@ -1,16 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
+import {Observable} from "rxjs";
+import {GithubCommitsInterface} from "@cl-test/github";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
-  private path = 'git-hub';
-  private url!:string;
+  private path = 'git-hub/';
+  private readonly url!:string;
   constructor(private readonly _httpClient: HttpClient) {
     this.url = `${environment.urlAPI}${this.path}`;
   }
 
-  getCommitsHistory(){}
+  getCommitsHistory(): Observable<GithubCommitsInterface>{
+    return this._httpClient.get<GithubCommitsInterface>(this.url+'commits');
+  }
 }
