754eaeabe234fa3bdebc4050e454fbe5022e3855
Cristian Lara <cristian89lara@gmail.com>
2022-10-29 12:20:21 -0500

    update: service handle error

--- cl-test/apps/api/src/git-hub/git-hub.service.ts
+++ cl-test/apps/api/src/git-hub/git-hub.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import {ConfigService} from '@nestjs/config';
 import {GithubClassBase} from './github.class';
 import {API_GITHUB_URL} from './github.constants';
+import {GithubCommitsInterface} from "@cl-test/github";
 @Injectable()
 export class GitHubService extends GithubClassBase{
   constructor(private readonly configServiceVariable: ConfigService) {
     super(configServiceVariable)
   }
-  async getCommitByRepo(){
-    return await this.octokit.request(API_GITHUB_URL.getCommits, this.repoCredentials);
+  async getCommitByRepo():Promise<GithubCommitsInterface>{
+    try {
+      const response = await this.octokit.request(API_GITHUB_URL.getCommits, this.repoCredentials)
+      return  response.data as GithubCommitsInterface ;
+    }catch (e){
+      throw new BadRequestException(e)
+    }
   }
 }
