74c4146d87f2b61411a04a98dde53bcbec85551a
Cristian Lara <cristian89lara@gmail.com>
2022-10-28 21:36:22 -0500

    feat: table component created

---
+++ cl-test/apps/front-app/src/app/components/table-commits/table-commits.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TableCommitsComponent } from './table-commits.component';
+
+describe('TableCommitsComponent', () => {
+  let component: TableCommitsComponent;
+  let fixture: ComponentFixture<TableCommitsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TableCommitsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TableCommitsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ cl-test/apps/front-app/src/app/components/table-commits/table-commits.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cl-test-table-commits',
+  templateUrl: './table-commits.component.html',
+  styleUrls: ['./table-commits.component.scss'],
+})
+export class TableCommitsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
