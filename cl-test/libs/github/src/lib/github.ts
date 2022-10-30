7aeee2565b61e3c75a365ee2f2b641ca3f348598
Cristian Lara <cristian89lara@gmail.com>
2022-10-29 21:59:46 -0600

    feat: partial interfaces created

--- cl-test/libs/github/src/lib/github.ts
+++ cl-test/libs/github/src/lib/github.ts
@@ -1,5 +1,31 @@
 interface GithubCommitsInterface {
-  nombre?: string;
+  sha?: string;
+  node_id?: string;
+  commit?: CommitInterface
+}
+interface CommitInterface {
+author: AuthorCommitInterface;
+committer: AuthorCommitInterface;
+message: string;
+tree: CommitTreeInterface;
+url: string;
+comment_count: number;
+verification: VerificationCommitInterface;
+}
+interface AuthorCommitInterface {
+  name: string;
+  email: string;
+  date: string;
+}
+interface CommitTreeInterface {
+  sha:string;
+  url:string;
+}
+interface VerificationCommitInterface{
+  verified: boolean;
+  reason: string;
+  signature: any;
+  payload: any;
 }
 export {
   GithubCommitsInterface
