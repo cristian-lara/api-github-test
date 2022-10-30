c0cc0bd09305cab2d3b35c60ac3e011e3c083b45
Cristian Lara <cristian89lara@gmail.com>
2022-10-29 23:33:00 -0500

    update: interfaces updated

--- cl-test/libs/github/src/lib/github.ts
+++ cl-test/libs/github/src/lib/github.ts
@@ -1,7 +1,33 @@
 interface GithubCommitsInterface {
   sha?: string;
   node_id?: string;
-  commit?: CommitInterface
+  commit?: CommitInterface;
+  url?: string;
+  html_url:string;
+  comments_url: string;
+  author: AuthorGithubInterface;
+  committer: AuthorGithubInterface;
+  parents: CommitTreeInterface[];
+}
+interface AuthorGithubInterface {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
 }
 interface CommitInterface {
 author: AuthorCommitInterface;
@@ -20,12 +46,13 @@ interface AuthorCommitInterface {
 interface CommitTreeInterface {
   sha:string;
   url:string;
+  html_url?:string;
 }
 interface VerificationCommitInterface{
   verified: boolean;
   reason: string;
-  signature: any;
-  payload: any;
+  signature: string;
+  payload: string;
 }
 export {
   GithubCommitsInterface
