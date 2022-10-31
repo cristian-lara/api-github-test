411b967364ed828798d420c55861ba4162a95c60
Cristian Lara <cristian89lara@gmail.com>
2022-10-30 20:33:22 -0500

    update: service updated

--- cl-test/apps/api/src/git-hub/git-hub.service.ts
+++ cl-test/apps/api/src/git-hub/git-hub.service.ts
@@ -9,11 +9,10 @@ export class GitHubService extends GithubClassBase{
   constructor(private readonly configServiceVariable: ConfigService) {
     super(configServiceVariable)
   }
-  async getCommitByRepo(perPage =10, page= 1):Promise<{ total: number; data: GithubCommitsInterface[] }>{
+  async getCommitByRepo(perPage =10, page= 1):Promise< GithubCommitsInterface[] >{
     try {
       const paginate = await this.octokit.request(`${API_GITHUB_URL.getCommits}`, {...this.repoCredentials, per_page:perPage,page})
-      const total = await this.octokit.request(`${API_GITHUB_URL.getCommits}`, {...this.repoCredentials})
-      return {data: paginate.data as GithubCommitsInterface[],total: total.data.length} ;
+      return paginate.data as GithubCommitsInterface[] ;
     }catch (e){
       throw new BadRequestException(e)
     }
